3f91c7f2a9b54413fb341d331dfdbfcee3afd5d8
unduashe <unduashe@outlook.es>
2024-11-21 13:07:59 +0000

    funcionalidad de cuenta atrás añadida, revisión de lógica para corrección de bugs y manejo de situaciones inesperadas

--- src/js/component/modal.jsx
+++ src/js/component/modal.jsx
@@ -1,36 +1,52 @@
 import React from "react";
-import { validaNumeros, establecerSegundosObjetivo } from "..";
+import { validaNumeros, establecerSegundosObjetivo, useDropDown } from "..";
 
 function Modal(){
+    let {dropdownText, dropdownHandler} = useDropDown();
+    
     return(
         <>
-            {/* <!-- Button trigger modal --> */}
+            {/* botón habilitador del modal */}
             <button type="button" className="col-12  btn btn-dark my-3 px-0 py-4 border-5 border-light-subtle fs-1 fw-bolder" data-bs-toggle="modal" data-bs-target="#modalNumeroObjetivo">
             <i className="fa-regular fa-clock"></i>
             </button>
-
-            {/* <!-- Modal --> */}
+            {/* fin de botón habilitador del modal */}
+            {/* inicio modal */}
             <div className="modal fade" id="modalNumeroObjetivo" tabIndex="-1" aria-labelledby="modalNumeroObjetivoLabel" aria-hidden="true">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                <div className="modal-header">
-                    <h1 className="modal-title fs-5" id="modalNumeroObjetivoLabel">Establecer número objetivo</h1>
-                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-                    <p>
-                    Por favor ingrese un número objetivo para que una vez alcanzado se le alerte: 
-                    </p>
-                    <input id="numeroObjetivo" className="w-100" type="text" placeholder="Número" maxLength={6} onKeyPress={validaNumeros}/>
-                </div>
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary" onClick={establecerSegundosObjetivo}>Save changes</button>
-                </div>
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="modalNumeroObjetivoLabel">Establecer número objetivo</h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        <p className="mb-2">
+                        Por favor seleccione modo e introduzca un número para que se le alerte cuando llegue a la cifra objetivo o cuando la cuenta atrás llegue a 0.
+                        </p>
+                        {/* inicio de dropdown para elegir el método de contador */}
+                        <div className="d-flex align-items-center">
+                            <label className="mb-3 me-2" htmlFor="btn-group">Modo:</label>
+                            <div className="btn-group">
+                                <button className="btn btn-secondary btn-sm dropdown-toggle mb-3" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    {dropdownText}
+                                </button>
+                                <ul className="dropdown-menu">
+                                    <li><span className="dropdown-item" onClick={dropdownHandler}>Contador</span></li>
+                                    <li><span className="dropdown-item" onClick={dropdownHandler}>Cuenta atrás</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                        {/* fin de dropdown para elegir el métido de contador */}
+                        <input id="numeroObjetivo" className="w-100" type="text" placeholder="Segundos" maxLength={6} onKeyPress={validaNumeros}/>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary" onClick={establecerSegundosObjetivo}>Save changes</button>
+                    </div>
+                    </div>
                 </div>
             </div>
-            </div>
-
+            {/* fin modal */}
         </>
     )
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
 
 // include your styles into the webpack bundle
@@ -15,7 +15,16 @@ const root = ReactDOM.createRoot(document.getElementById("app"));
 //variable que almacena el contador del número que se ira actualizando cada segundo
 let segundos = 0;
 
+//se define variable globalmente para poder ser llamada en funciones de modificación de comportamiento del contador
+let intervalo = null;
 let segundosObjetivo = null;
+let dropDownValue = "Contador";
+//esta variable es utilizada para que el funcionamiento de la alarma tanto en cuenta atrás como en contador
+//funciona correctamente y no salta a destiempo
+let cuentaAtrasAlarma = null;
+
+//se llama a la funcion definida a continuación para que carguen los datos de la página
+CalculoEImpresionContador()
 
 //función para obtener y actualizar el número en el código html
 function CalculoEImpresionContador(){
@@ -35,54 +44,83 @@ function CalculoEImpresionContador(){
         numberCentenaDeMillar={centenaDeMillares}/>);
     if (segundos === 1_000_000) return (
         clearInterval(intervalo),
-        alert("Enhorabuena, llegaste hasta el final, actualiza la página para volver a empezar"));
-    else if (segundosObjetivo == segundos) return (
+        alert("Enhorabuena, llegaste hasta el final, actualiza la página para volver a empezar."));
+    else if (segundosObjetivo == segundos && !cuentaAtrasAlarma && dropDownValue === "Contador") return (
         clearInterval(intervalo),
-        alert(`Se ha alcanzado el segundo ${segundosObjetivo}`));
-        
+        alert(`Se ha alcanzado el segundo ${segundosObjetivo}.`));
+    else if (dropDownValue === "Cuenta atrás" && cuentaAtrasAlarma == 0 && segundos == 0) return (
+        clearInterval(intervalo),
+        cuentaAtrasAlarma = null,
+        alert(`Han trascurrido ${segundosObjetivo} segundos.`)); 
 }
-
-//se llama a la funcion recien definida para que carguen los datos de la página
-CalculoEImpresionContador()
-
-//se define variable globalmente para poder ser llamada en funciones de modificación de comportamiento del contador
-let intervalo = null;
 
 function escuchaEmpezar(){
     //se crea condifión if para que no se pueda iniciar varias veces el contador
     if (!intervalo) {
-        intervalo = setInterval(() =>{
-            segundos++;
-            CalculoEImpresionContador()
-        }, 1000);
+        //manejo de selección de cuenta atrás
+        if (dropDownValue === "Cuenta atrás" && segundosObjetivo > 0){
+            intervalo = setInterval(() =>{
+                segundos--;
+                CalculoEImpresionContador()
+            }, 1000);
+        }
+        // manejo de selecciónd de contador
+        else {
+            intervalo = setInterval(() =>{
+                segundos++;
+                CalculoEImpresionContador()
+            }, 1000);
+        }
     }
-    console.log("empezar");
 }
 
 function escuchaParar(){
-    //se elimina el intervalo generado en empezar y se devuelve la variable a null para que pueda volver a ser inicializada
+    //se elimina el intervalo generado en botón empezar y se devuelve la variable a null para que pueda volver a ser inicializada
     //desde botón empezar
     clearInterval(intervalo);
     intervalo = null
-    console.log("parar");
 }
 
 function escuchaReset(){
     escuchaParar()
     segundos = 0;
+    segundosObjetivo= null;
     CalculoEImpresionContador();
-    console.log("reset");
 }
 
+//funcion para validar que el input de segundos es un número
 function validaNumeros(e){
     if (!/[0-9]/.test(e.key)) 
         {e.preventDefault();}
 }
 
+
 function establecerSegundosObjetivo(){
-    escuchaReset()
+    escuchaReset();
     let numeroObjetivo = document.querySelector("#numeroObjetivo").value;
+    if (numeroObjetivo == 0) return alert("el tiempo establecido debe ser superior a 0");
     segundosObjetivo = numeroObjetivo;
+    // actualización de variables globales para el correcto funcionamiento de la alerta e información mostrada en pantalla
+    if (dropDownValue === "Cuenta atrás"){
+        segundos = segundosObjetivo;
+        cuentaAtrasAlarma = 0;
+    }
+    // actualización de variables globales para el correcto funcionamiento de la alerta e información mostrada en pantalla
+    else {
+        cuentaAtrasAlarma = null;
+        alert(`Tiempo establecido, cuando se alcance el segundo ${numeroObjetivo} se le alertará.`);
+    }
+    CalculoEImpresionContador()
 }
 
-export {escuchaEmpezar, escuchaParar, escuchaReset, validaNumeros, establecerSegundosObjetivo}
+//hook del modal para obtener el valor clicado y que se actualice en pantalla
+function useDropDown(){
+    let [dropdownText, setDropDownText] = useState("Contador");
+    let dropdownHandler = (e) =>{
+        setDropDownText(e.target.textContent);
+    }
+    dropDownValue = dropdownText;
+    return {dropdownText, dropdownHandler};
+}
+
+export {escuchaEmpezar, escuchaParar, escuchaReset, validaNumeros, establecerSegundosObjetivo, useDropDown}
